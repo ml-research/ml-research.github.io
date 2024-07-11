776cbdfcf9f25ca866534502ad1afec73bc87570
bluemlJ <bluemlJ@gmx.net>
2024-07-11 09:51:17 +0200

    add share button

---
+++ people/jblueml/papertrees/ocatari/script.js
@@ -0,0 +1,27 @@
+const shareButton = document.querySelector('.share-button');
+const shareDialog = document.querySelector('.share-dialog');
+const closeButton = document.querySelector('.close-button');
+
+shareButton.addEventListener('click', event => {
+  if (navigator.share) { 
+   navigator.share({
+      title: 'WebShare API Demo',
+      url: 'https://codepen.io/ayoisaiah/pen/YbNazJ'
+    }).then(() => {
+      console.log('Thanks for sharing!');
+    })
+    .catch(console.error);
+    } else {
+        shareDialog.classList.add('is-open');
+    }
+});
+
+closeButton.addEventListener('click', event => {
+  shareDialog.classList.remove('is-open');
+});
+
+function copy() {
+  var copyText = window.location.href;
+  navigator.clipboard.writeText(copyText);
+  shareDialog.classList.remove('is-open');
+}
