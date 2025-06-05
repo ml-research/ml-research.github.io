3ed3eda90b332f7daf00dfa48d9fc23f1c1fe8d4
BluemlJ <bluemlj@gmx.net>
2025-06-05 15:52:31 +0200

    adding papertree for comet

---
+++ people/jblueml/papertrees/comet/script.js
@@ -0,0 +1,26 @@
+const shareButton = document.querySelector('.share-button');
+const shareDialog = document.querySelector('.share-dialog');
+const closeButton = document.querySelector('.close-button');
+
+shareButton.addEventListener('click', event => {
+  if (navigator.share) { 
+   navigator.share({
+      url: window.location.href
+    }).then(() => {
+      console.log('Thanks for sharing!');
+    })
+    .catch(console.error);
+    } else {
+      shareDialog.classList.add('is-open');
+    }
+});
+
+closeButton.addEventListener('click', event => {
+  shareDialog.classList.remove('is-open');
+});
+
+function copy() {
+  var copyText = window.location.href;
+  navigator.clipboard.writeText(copyText);
+  shareDialog.classList.remove('is-open');
+}
